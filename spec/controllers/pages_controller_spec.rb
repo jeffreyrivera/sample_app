a0acc9da9aab3fe0028bd6128231ea2c4ae9dc00
Jeffrey Rivera <jeffreyrivera@gmail.com>
2011-04-21 13:21:18 -0400

    Done with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,19 +1,44 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
-  end
-
+  
+	it "should have a the right title" do
+		get 'home'
+		response.should have_selector("title", :content => "Ruby on rails Sample app | Home")
+	end
+  end	
+		
+		
+		
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
     end
+    
+    it "should have a the right title" do
+		get 'contact'
+		response.should have_selector("title", :content => "Ruby on rails Sample app | Contact")
+	end
+  end
+
+  describe "GET 'about'" do  #test for about first failing(red) then we create about views and actions.
+    it "should be successful" do
+      get 'about'
+      response.should be_success 
+    end
+    
+    it "should have a the right title" do
+		get 'about'
+		response.should have_selector("title", :content => "Ruby on rails Sample app | About")
+	end
   end
 
 end
