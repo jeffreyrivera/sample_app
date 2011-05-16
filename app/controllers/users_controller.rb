cae0e45875ac39951cb8a1916e3da54e9b9b491c
Jeffrey Rivera <jeffreyrivera@gmail.com>
2011-05-15 23:59:40 -0400

    end of model chapter 7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   end
   def show
     	@user = User.find(params[:id])
+    	@title = @user.name
   end
 
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+	def gravatar_for (user, options = { :size => 50 })
+		#check gravatar documentation for more
+		gravatar_image_tag(user.email.downcase, :alt => user.name,
+												:class => 'gravatar',
+												:gravatar => options)
+	end
 end
