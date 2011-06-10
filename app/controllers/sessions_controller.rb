de023b515d8e7884dcea2c2a56396a7293512f72
Jeffrey Rivera <jeffreyrivera@gmail.com>
2011-06-10 13:35:27 -0400

    sign in completed

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+
+  def new
+  	@title = "Sign in"
+  end
+  
+  def create
+	
+	user = User.authenticate(params[:session][:email],
+							 params[:session][:password])
+	if user.nil?
+	
+		flash.now[:error] = "Invalid email/password combination."
+		@title = "Sign in"
+		render 'new'
+		#create an error message and re-render the signin form
+	else
+		sign_in user
+		redirect_to user	
+		#sign in user and redirect to the users show page
+	end
+	
+  end
+  
+  def destroy
+  
+  end
+
+end
